f31021f295b191c68a55b3280aea3090c76feae9
lucas rodrigues <rodrigueslucas062@gmail.com>
2023-09-01 12:58:22 -0300

    feat: Botões com efeito e estilizados

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight } from "lucide-react"
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play } from "lucide-react"
 import Image from "next/image"
 import Curtidas from "../../public/images/album/curtidas.jpg"
 import Samurai from "../../public/images/album/chippin-in.jpg"
@@ -11,15 +11,15 @@ const renderAside = () => {
     return (
         <aside className="w-72 bg-zinc-950 p-6">
             <nav className="space-y-5">
-                <a href="" className="flex items-center gap-3 text-xs font-semibold text-zinc-200">
+                <a href="" className="flex items-center gap-3 text-xs font-semibold text-zinc-300 hover:text-neutral-50">
                     <HomeIcon />
                     Home
                 </a>
-                <a href="" className="flex items-center gap-3 text-xs font-semibold text-zinc-200">
+                <a href="" className="flex items-center gap-3 text-xs font-semibold text-zinc-300 hover:text-neutral-50">
                     <Search />
                     Search
                 </a>
-                <a href="" className="flex items-center gap-3 text-xs font-semibold text-zinc-200">
+                <a href="" className="flex items-center gap-3 text-xs font-semibold text-zinc-300 hover:text-neutral-50">
                     <Library />
                     Library
                 </a>
@@ -43,7 +43,7 @@ const renderAside = () => {
 const renderMain = () => {
     return (
         <main className="flex-1 p-6">
-            <div className="flex items-center gap-4 mt-4">
+            <div className="flex items-center gap-4 mt-2">
                 <button className="rounded-full bg-black/40 p-2 ">
                     <ChevronLeft />
                 </button>
@@ -51,31 +51,49 @@ const renderMain = () => {
                     <ChevronRight />
                 </button>
             </div>
-            <h1 className="font-semi-bold text-3xl mt-10">Bom dia</h1>
+            <h1 className="font-bold text-3xl mt-10 gap-4">Bom dia</h1>
             <div className="grid grid-cols-3 gap-4">
-                <a href="" className="bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
+                <a href="" className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
                     <Image src={Curtidas} alt="Capa playlist" width={90} height={90} />
                     <strong>Músicas Curtidas</strong>
+                    <button className="w-12 h-12 flex items-center justify-center rounded-full pl-1 bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                        <Play fill="black"/>
+                    </button>
                 </a>
-                <a href="" className="bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
+                <a href="" className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
                     <Image src={Samurai} alt="Capa playlist" width={90} height={90} />
                     <strong>Chippin' In</strong>
+                    <button className="w-12 h-12 flex items-center justify-center rounded-full pl-1 bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                        <Play fill="black"/>
+                    </button>
                 </a>
-                <a href="" className="bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
+                <a href="" className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
                     <Image src={RiseRise} alt="Capa playlist" width={90} height={90} />
                     <strong>Rise, Rise</strong>
+                    <button className="w-12 h-12 flex items-center justify-center rounded-full pl-1 bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                        <Play fill="black"/>
+                    </button>
                 </a>
-                <a href="" className="bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
+                <a href="" className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
                     <Image src={Tdi} alt="Capa playlist" width={90} height={90} />
                     <strong>The Devil In I</strong>
+                    <button className="w-12 h-12 flex items-center justify-center rounded-full pl-1 bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                        <Play fill="black"/>
+                    </button>
                 </a>
-                <a href="" className="bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
+                <a href="" className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
                     <Image src={Archive} alt="Capa playlist" width={90} height={90} />
                     <strong>Bullets</strong>
+                    <button className="w-12 h-12 flex items-center justify-center rounded-full pl-1 bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                        <Play fill=""/>
+                    </button>
                 </a>
-                <a href="" className="bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
+                <a href="" className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
                     <Image src={Aderal} alt="Capa playlist" width={90} height={90} />
                     <strong>Aderal</strong>
+                    <button className="w-12 h-12 flex items-center justify-center rounded-full pl-1 bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                        <Play fill="black"/>
+                    </button>
                 </a>
             </div>
         </main>
