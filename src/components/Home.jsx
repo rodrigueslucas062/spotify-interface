d35163d25fc48dc568cea15bce968ab27fa91855
lucas rodrigues <rodrigueslucas062@gmail.com>
2023-09-04 00:42:19 -0300

    feat: Alteração na home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -40,7 +40,7 @@ const renderAside = () => {
     )
 }
 
-const renderMain = () => {
+const renderMainPlay = () => {
     return (
         <main className="flex-1 p-6">
             <div className="flex items-center gap-4 mt-2">
@@ -57,56 +57,87 @@ const renderMain = () => {
                     <Image src={Curtidas} alt="Capa playlist" width={90} height={90} />
                     <strong>Músicas Curtidas</strong>
                     <button className="w-12 h-12 flex items-center justify-center rounded-full pl-1 bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
-                        <Play fill="black"/>
+                        <Play fill="black" />
                     </button>
                 </a>
                 <a href="" className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
                     <Image src={Samurai} alt="Capa playlist" width={90} height={90} />
                     <strong>Chippin' In</strong>
                     <button className="w-12 h-12 flex items-center justify-center rounded-full pl-1 bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
-                        <Play fill="black"/>
+                        <Play fill="black" />
                     </button>
                 </a>
                 <a href="" className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
                     <Image src={RiseRise} alt="Capa playlist" width={90} height={90} />
                     <strong>Rise, Rise</strong>
                     <button className="w-12 h-12 flex items-center justify-center rounded-full pl-1 bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
-                        <Play fill="black"/>
+                        <Play fill="black" />
                     </button>
                 </a>
                 <a href="" className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
                     <Image src={Tdi} alt="Capa playlist" width={90} height={90} />
                     <strong>The Devil In I</strong>
                     <button className="w-12 h-12 flex items-center justify-center rounded-full pl-1 bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
-                        <Play fill="black"/>
+                        <Play fill="black" />
                     </button>
                 </a>
                 <a href="" className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
                     <Image src={Archive} alt="Capa playlist" width={90} height={90} />
                     <strong>Bullets</strong>
                     <button className="w-12 h-12 flex items-center justify-center rounded-full pl-1 bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
-                        <Play fill=""/>
+                        <Play fill="" />
                     </button>
                 </a>
                 <a href="" className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
                     <Image src={Aderal} alt="Capa playlist" width={90} height={90} />
                     <strong>Aderal</strong>
                     <button className="w-12 h-12 flex items-center justify-center rounded-full pl-1 bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
-                        <Play fill="black"/>
+                        <Play fill="black" />
                     </button>
                 </a>
+            </div>
+            <h2 className="font-bold text-2xl mt-10">Feito para você</h2>
+            <div className="ml-4">
+                <div className="grid grid-cols-6 gap-4 mt-4">
+                    <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+                        <Image className="w-auto" src={Aderal} alt="Capa playlist" width={190} height={190} />
+                        <strong className="font-semi-bold">Aderal</strong>
+                        <span className="text-sm text-zinc-500">Aderal, de Slipknot</span>
+                    </a>
+                    <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+                        <Image className="w-auto" src={Archive} alt="Capa playlist" width={190} height={190} />
+                        <strong className="font-semi-bold">Bullets</strong>
+                        <span className="text-sm text-zinc-500">Bullets, de Archive</span>
+                    </a>
+                    <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+                        <Image className="w-auto" src={RiseRise} alt="Capa playlist" width={190} height={190} />
+                        <strong className="font-semi-bold">Rise, rise</strong>
+                        <span className="text-sm text-zinc-500">Rise, Rise, de Rammstein</span>
+                    </a>
+                    <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+                        <Image className="w-auto" src={Tdi} alt="Capa playlist" width={190} height={190} />
+                        <strong className="font-semi-bold">The devil in i</strong>
+                        <span className="text-sm text-zinc-500">The Devil in I, de Slipknot</span>
+                    </a>
+                    <a href="" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+                        <Image className="w-auto" src={Samurai} alt="Capa playlist" width={190} height={190} />
+                        <strong className="font-semi-bold">Chippin' in</strong>
+                        <span className="text-sm text-zinc-500">Chippin' in, de Samurai</span>
+                    </a>
+                </div>
             </div>
         </main>
     )
 }
+
 const Home = () => {
     return (
         <div className="h-screen flex flex-col">
             <div className="flex flex-1">
-                {/* Area aside */}
+                {/* Aside area*/}
                 {renderAside()}
-                {/* Area main */}
-                {renderMain()}
+                {/* Main area*/}
+                {renderMainPlay()}
             </div>
             <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
                 footer
