b9d34256c06eae8f14673f40133dacd295030757
lucas rodrigues <rodrigueslucas062@gmail.com>
2023-09-07 00:00:49 -0300

    feat: Inicio implementação do board

--- src/components/whiteboard/Whiteboard.jsx
+++ src/components/whiteboard/Whiteboard.jsx
@@ -1,11 +1,51 @@
-import ReactFlow, { Controls, Background } from 'reactflow';
+import ReactFlow, { Controls, Background, useEdgesState } from 'reactflow';
 import 'reactflow/dist/style.css';
+import { zinc } from 'tailwindcss/colors'
+import { Square } from './nodes/Square';
+import { useCallback } from 'react';
 
 const Whiteboard = () => {
+    const NODE_TYPES = {
+        square: Square,
+    }
+
+    const INITIAL_NODES = [
+        {
+            id: crypto.randomUUID(),
+            type: 'square',
+            position: {
+                x: 400,
+                y: 200,
+            },
+            data: {},
+        },
+        {
+            id: crypto.randomUUID(),
+            type: 'square',
+            position: {
+                x: 800,
+                y: 200,
+            },
+            data: {},
+        }
+    ]
+
+    const [edges, SetEdges, onEdgesChange] = useEdgesState([])
+
+    const onConnect = useCallback((connection) => {
+        return SetEdges(edges => addEdge(connection, edges))
+    }, [])
+
     return (
-        <div style={{ width: '100vw', height: '100vh' }}>
-            <ReactFlow >
-                <Background />
+        <div className='w-screen h-screen'>
+            <ReactFlow
+                nodeTypes={NODE_TYPES}
+                nodes={INITIAL_NODES}
+                edges={edges}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
+                connectionMode="loose">
+                <Background color={zinc[200]} />
                 <Controls />
             </ReactFlow>
         </div>
