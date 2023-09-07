78aeeaa1d67b288b28eae9b1e1d69dde1f38ccf4
lucas rodrigues <rodrigueslucas062@gmail.com>
2023-09-07 13:03:24 -0300

    fix: Board guarda o estado das linhas

--- src/components/whiteboard/Whiteboard.jsx
+++ src/components/whiteboard/Whiteboard.jsx
@@ -1,40 +1,41 @@
-import ReactFlow, { Controls, Background, useEdgesState } from 'reactflow';
+import ReactFlow, { Controls, Background, addEdge} from 'reactflow';
 import 'reactflow/dist/style.css';
 import { zinc } from 'tailwindcss/colors'
 import { Square } from './nodes/Square';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
+
+const INITIAL_NODES = [
+    {
+        id: crypto.randomUUID(),
+        type: 'square',
+        position: {
+            x: 400,
+            y: 200,
+        },
+        data: {},
+    },
+    {
+        id: crypto.randomUUID(),
+        type: 'square',
+        position: {
+            x: 800,
+            y: 200,
+        },
+        data: {},
+    }
+]
+
+const initialEdges = [];
 
 const Whiteboard = () => {
+    const [nodes, setNodes] = useState(INITIAL_NODES);
+    const [edges, setEdges] = useState(initialEdges);
+
     const NODE_TYPES = {
         square: Square,
     }
 
-    const INITIAL_NODES = [
-        {
-            id: crypto.randomUUID(),
-            type: 'square',
-            position: {
-                x: 400,
-                y: 200,
-            },
-            data: {},
-        },
-        {
-            id: crypto.randomUUID(),
-            type: 'square',
-            position: {
-                x: 800,
-                y: 200,
-            },
-            data: {},
-        }
-    ]
-
-    const [edges, SetEdges, onEdgesChange] = useEdgesState([])
-
-    const onConnect = useCallback((connection) => {
-        return SetEdges(edges => addEdge(connection, edges))
-    }, [])
+    const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
 
     return (
         <div className='w-screen h-screen'>
@@ -42,7 +43,6 @@ const Whiteboard = () => {
                 nodeTypes={NODE_TYPES}
                 nodes={INITIAL_NODES}
                 edges={edges}
-                onEdgesChange={onEdgesChange}
                 onConnect={onConnect}
                 connectionMode="loose">
                 <Background color={zinc[200]} />
