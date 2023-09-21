b91bbb417d23e471dc6aa29da288830e69a18cde
lucas rodrigues <rodrigueslucas062@gmail.com>
2023-09-20 23:04:34 -0300

    feat: Botão de navegação entre páginas

--- utils/navegacao/NavButton.jsx
+++ utils/navegacao/NavButton.jsx
@@ -1,12 +1,43 @@
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 import { ChevronLeft, ChevronRight } from "lucide-react"
 
 const NavButton = () => {
+    const router = useRouter()
+    const [ultimaRota, setUltimaRota] = useState(null)
+
+    const handleAvancar = () => {
+        if (ultimaRota) {
+            router.push(ultimaRota)
+        }
+    }
+
+    const handleVoltar = () => {
+        router.back()
+    }
+
+    useEffect(() => {
+        setUltimaRota(router.asPath)
+    }, [])
+
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            setUltimaRota(url)
+        }
+
+        router.events.on('routeChangeComplete', handleRouteChange)
+
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router])
+
     return (
         <div className="flex items-center gap-4 mt-2">
-            <button className="rounded-full bg-black/40 p-2 hover:bg-white/10">
+            <button className="rounded-full bg-black/40 p-2 hover:bg-white/10" onClick={handleVoltar}>
                 <ChevronLeft />
             </button>
-            <button className="rounded-full bg-black/40 p-2 hover:bg-white/10">
+            <button className="rounded-full bg-black/40 p-2 hover:bg-white/10" onClick={handleAvancar}>
                 <ChevronRight />
             </button>
         </div>
