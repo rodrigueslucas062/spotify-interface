eaa6f821a14c32934c039d95f401849197e080fc
lucas rodrigues <rodrigueslucas062@gmail.com>
2023-09-27 00:16:50 -0300

    feat: Novo componente

---
+++ components/ficha/Ficha.jsx
@@ -0,0 +1,10 @@
+
+const Ficha = () => {
+    return(
+        <div>
+            <span>em construção...</span>
+        </div>
+    )
+}
+
+export default Ficha;
---
+++ pages/personagem.js
@@ -0,0 +1,11 @@
+import Ficha from "../components/ficha/Ficha";
+
+const Personagem = () => {
+  return (
+    <>
+      <Ficha />
+    </>
+  );
+};
+
+export default Personagem
