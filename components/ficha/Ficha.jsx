1d89c66556430471586ecbc9671eb2e366be9a6e
lucas rodrigues <rodrigueslucas062@gmail.com>
2023-10-13 01:00:47 -0300

    feaat: Finalização primeira sessão da ficha

--- components/ficha/Ficha.jsx
+++ components/ficha/Ficha.jsx
@@ -5,6 +5,121 @@ const Ficha = () => {
     const papeis = criarDivsComClasses(classes)
     const atributo = criarDivsComAtributos(atributos)
 
+    const renderBoxArea1 = () => {
+        return (
+            <div className="space-y-4">
+                <div className="flex d-flex gap-4">
+                    <div className="w-1/5">
+                        <span>Leve</span>
+                        <div className="flex">
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                        </div>
+                    </div>
+                    <div className="w-1/5">
+                        <span>Grave</span>
+                        <div className="flex">
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                        </div>
+                    </div>
+                    <div className="w-1/5">
+                        <span>Critico</span>
+                        <div className="flex">
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                        </div>
+                    </div>
+                    <div className="w-1/5">
+                        <span>Mortal-0</span>
+                        <div className="flex">
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                        </div>
+                    </div>
+                    <div className="w-1/5">
+                        <span>Mortal-1</span>
+                        <div className="flex">
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex border border-gray-400">
+                    <p className="p-2 w-full">Atord=0</p>
+                    <p className="p-2 w-full">Atord=1</p>
+                    <p className="p-2 w-full">Atord=2</p>
+                    <p className="p-2 w-full">Atord=3</p>
+                    <p className="p-2 w-full">Atord=4</p>
+                </div>
+                <div className="flex space-x-2">
+                    <div className="w-1/5">
+                        <span>Mortal-2</span>
+                        <div className="flex">
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                        </div>
+                    </div>
+                    <div className="w-1/5">
+                        <span>Mortal-3</span>
+                        <div className="flex">
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                        </div>
+                    </div>
+                    <div className="w-1/5">
+                        <span>Mortal-4</span>
+                        <div className="flex">
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                        </div>
+                    </div>
+                    <div className="w-1/5">
+                        <span>Mortal-5</span>
+                        <div className="flex">
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                        </div>
+                    </div>
+                    <div className="w-1/5">
+                        <span>Mortal-6</span>
+                        <div className="flex">
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                            <div className="w-1/4 border border-gray-400 p-2"></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex border border-gray-400">
+                    <p className="p-2 w-full">Atord=5</p>
+                    <p className="p-2 w-full">Atord=6</p>
+                    <p className="p-2 w-full">Atord=7</p>
+                    <p className="p-2 w-full">Atord=8</p>
+                    <p className="p-2 w-full">Atord=9</p>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <form action="" className="py-12 mx-40">
             <div className="d-flex flex gap-5 p-5">
@@ -41,34 +156,18 @@ const Ficha = () => {
                             </div>
                         </div>
                     </div>
+                    <div className="flex space-x-2">
+                        <div className="flex flex-col border border-gray-400 p-3">
+                            <span>VIT</span>
+                        </div>
+                        <div className="flex flex-col border border-gray-400 p-3">
+                            <span>MTC</span>
+                        </div>
+                        {renderBoxArea1()}
+                    </div>
                     <div>
-                        <div>
-                            <span>VIT</span> <span>MTC</span>
-                        </div>
-                        <div>
-                            <span>Leve</span>
-                            <span>Grave</span>
-                            <span>Critico</span>
-                            <span>Mortal-0</span>
-                            <span>mortal-1</span>
-                        </div>
-                        <div>
-                            <p>Atord=0 Atord=1 Atord=2 Atord=3 Atord=4</p>
-                        </div>
-                        <div>
-                            <span>mortal-2</span>
-                            <span>mortal-3</span>
-                            <span>mortal-4</span>
-                            <span>mortal-5</span>
-                            <span>mortal-6</span>
-                        </div>
-                        <div>
-                            <p>Atord=4 Atord=6 Atord=7 Atord=8 Atord=9</p>
-                        </div>
-                        <div>
-                            <span>PERÍCIAS</span>
-                            <p>Somente os pontos de cada Perícia como Atributo correspondente e amote-os no campo correto. Marque com um X ao lado dos colchetes as pericias por Chips</p>
-                        </div>
+                        <span>PERÍCIAS</span>
+                        <p>Somente os pontos de cada Perícia como Atributo correspondente e amote-os no campo correto. Marque com um X ao lado dos colchetes as pericias por Chips</p>
                     </div>
                 </div>
                 <div className="w-1/2 space-y-4">
