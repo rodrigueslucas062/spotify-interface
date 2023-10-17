f53e5c74ab8f6d17bdb00bfcfd84e5643c90b100
lucas rodrigues <rodrigueslucas062@gmail.com>
2023-10-17 11:48:53 -0300

    feat: Estilização Aside

--- components/Home/Aside.jsx
+++ components/Home/Aside.jsx
@@ -3,33 +3,33 @@ const { HomeIcon, Search, Library, GalleryThumbnails, Gamepad2,  } = require("lu
 
 const renderAside = () => {
     return (
-        <aside className="flex-2 w-72 flex flex-col justify-between">
+        <aside className="aside__area flex-2">
             <div className="bg-zinc-950 space-y-3">
-                <div className="bg-zinc-800 space-y-1 rounded-lg">
-                    <a href="" className="flex items-center p-3 gap-3 text-xs font-semibold text-zinc-300 hover:text-neutral-50">
+                <div className="aside_menu space-y-1">
+                    <Link href="" className="text-xs font-semibold text-zinc-300 hover:text-neutral-50">
                         <HomeIcon />
                         Home
-                    </a>
-                    <a href="" className="flex items-center p-3 gap-3 text-xs font-semibold text-zinc-300 hover:text-neutral-50">
+                    </Link>
+                    <Link href="" className="text-xs font-semibold text-zinc-300 hover:text-neutral-50">
                         <Search />
                         Pesquisar
-                    </a>
+                    </Link>
                 </div>
             </div>
             <div className="bg-zinc-800 space-y-1 p-2 rounded-t-lg mt-3">
-                <a href="" className="flex items-center gap-3 p-1 text-xs font-semibold text-zinc-300 hover:text-neutral-50">
+                <Link href="" className="flex items-center gap-3 p-1 text-xs font-semibold text-zinc-300 hover:text-neutral-50">
                     <Library />
                     Galeria
-                </a>
+                </Link>
             </div>
             <div className="flex-grow bg-neutral-900 space-y-1 p-2 rounded-b-lg">
-                <div className="flex flex-col gap-3">
-                    <Link href="/whiteboard"  className="group rounded flex items-center gap-4 p-2 overflow-hidden hover:bg-zinc-800 transition-colors">
+                <div className="aside_item">
+                    <Link href="/whiteboard"  className="group hover:bg-zinc-800 transition-colors">
                     <GalleryThumbnails />Blackboard
                     </Link>
                 </div>
-                <div className="flex flex-col gap-3">
-                    <Link href="/personagem"  className="group rounded flex items-center gap-4 p-2 overflow-hidden hover:bg-zinc-800 transition-colors">
+                <div className="aside_item">
+                    <Link href="/personagem"  className="group overflow-hidden hover:bg-zinc-800 transition-colors">
                     <Gamepad2 />Ficha
                     </Link>
                 </div>
